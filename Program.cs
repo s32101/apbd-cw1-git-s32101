92e5b35d2dfdb856dd36860184f2049fb979dc5f
Bartek <s32101@pjwstk.edu.pl>
2026-03-11 10:42:36 +0100

    nazwa programu

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, PJATK!");
+Console.WriteLine();
+
+Console.WriteLine("Super Kalkulator 6000 PRO MAX Ultra Galaxy 2")
